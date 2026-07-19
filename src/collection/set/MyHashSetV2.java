87a20a5b4200f3d3b4afe844082fa47d50e5c76f
username2015 <l030105@naver.com>
2026-07-19 22:29:08 +0900

    feat: HashSet 공부

---
+++ src/collection/set/MyHashSetV2.java
@@ -0,0 +1,65 @@
+package collection.set;
+
+import java.util.LinkedList;
+
+public class MyHashSetV1 {
+
+    static final int  DEFAULT_INITIAL_CAPACITY = 16;
+
+    LinkedList<Integer>[] buckets;
+
+    private int size = 0;
+    private int capacity = DEFAULT_INITIAL_CAPACITY;
+
+    public MyHashSetV1() {
+        initBuckets();
+    }
+
+    public MyHashSetV1(int capacity) {
+        this.capacity = capacity;
+        initBuckets();
+    }
+
+    private void initBuckets() {
+        buckets = new LinkedList[capacity];
+        for (int i = 0; i < capacity; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    public boolean add(int value){
+        int hashIndex = hashIndex(value);
+        LinkedList<Integer> bucket = buckets[hashIndex];
+        if(bucket.contains(value)){
+            return false;
+        }
+        bucket.add(value);
+        size++;
+        return true;
+    }
+
+    public boolean contains(int searchvalue){
+        int hashIndex = hashIndex(searchvalue);
+        LinkedList<Integer> bucket = buckets[hashIndex];
+        return bucket.contains(searchvalue);
+    }
+    public  boolean remove(int value){
+        int hashIndex = hashIndex(value);
+        LinkedList<Integer> bucket = buckets[hashIndex];
+        boolean result =bucket.remove(Integer.valueOf(value));
+        if(result){
+            size--;
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+
+    private int hashIndex(int value){
+        return  value %capacity;
+    }
+
+
+}
+
---
+++ src/collection/set/MyHashSetV2Main1.java
@@ -0,0 +1,4 @@
+package collection.set;
+
+public class MyHashSetV2Main1 {
+}
